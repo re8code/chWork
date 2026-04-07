7345eb7f6bd4177569bb605f00f0b30390359b7d
re8code <won@re8code.com>
2026-04-07 13:14:29 +0900

    #07-apply-3

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+int num[100];
+void swap(int s, int e) {
+    int t = num[s];
+    num[s] = num[e];
+    num[e] = t;
+}
 int main() {
-    string address, city, area, street, building;
+    // Mix
+    for(int i=0; i<100; i++) num[i] = i; // 인덱스 구성
 
-    cout << "시 : "; cin >> city;
-    cout << "구 : "; cin >> area;
-    cout << "로 : "; cin >> street;
-    cout << "건물명 : "; cin >> building;
-    address = city+area+street+building;
-    cout << address;
+    srand(time(NULL));
+    int e = 100;
+    while(e>0) {
+        // 비중복 랜덤 재구성 진행
+        int s = rand()%e;
+        swap(s, (e--)-1);
+    }
+
+    // Result
+    ofstream fout("temp.txt");
+    for(int i=0; i<100; i++) {
+        fout << num[i] << ' ';
+        if(i%10 == 9) fout << endl;
+    }
+    fout.close();
 
     return 0;
 }
