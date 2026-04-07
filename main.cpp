c023f9258ddea6bb2729f7bab7a7eefc254d0e1d
re8code <won@re8code.com>
2026-04-07 10:05:06 +0900

    #07-apply-1

--- main.cpp
+++ main.cpp
@@ -2,21 +2,14 @@
 using namespace std;
 
 int main() {
-    string data = "사랑,프로그래밍,의자,사랑의바보,영통역,천년의사랑,냉장고,객체지향";
-    string search;
-    string res = "";
+    string address, city, area, street, building;
 
-    cout << "키워드 : "; cin >> search;
+    cout << "시 : "; cin >> city;
+    cout << "구 : "; cin >> area;
+    cout << "로 : "; cin >> street;
+    cout << "건물명 : "; cin >> building;
+    address = city+area+street+building;
+    cout << address;
 
-    int start=0, end=0, pos=0;
-    while(pos != data.length()) {
-        pos = data.find(',', start);
-        if(pos==string::npos) pos=data.length();
-
-        string word = data.substr(start, pos-start);
-        if(word.find(search) != string::npos)
-            res += (res=="" ? "" : ",") + word;
-        start = pos+1;
-    }
-    cout <<  "검색결과 : " <<  res << endl;
+    return 0;
 }
