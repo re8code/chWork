2a15c248f36043e743b47f438b8e677e98c892b0
re8code <won@re8code.com>
2026-04-09 13:10:47 +0900

    #08-start

--- main.cpp
+++ main.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 int main() {
-    ifstream fin("text5.txt");
-
-    string line;
-    char res[1000] = {};
-    int offset=0;
-
-    while(getline(fin, line)) {
-        strcpy(res+offset, line.c_str());
-        res[offset+line.length()] = ' ';
-        offset += line.length()+1;
-    }
-    fin.close();
-
-    int len;
-    cout << "length = "; cin >> len;
-
-    ofstream fout("output.txt");
-    string result = string(res);
-    for(int i=0; i<result.length(); i++) {
-        fout << result[i];
-        if(i%len==len-1) fout << endl;
-    }
-    fout.close();
-
     return 0;
 }
